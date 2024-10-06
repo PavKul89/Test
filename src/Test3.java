f3501cce2ac3c68e31a85f9773b237be2fde1e65
Pavel <PavKul89@gmail.com>
2024-10-06 12:57:06 +0300

    add a lot DOPAP

--- src/Test3.java
+++ src/Test3.java
@@ -2,6 +2,12 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("Others");
         System.out.println("Others 2");
+        System.out.println("Others 2");
+        System.out.println("Others 2");
+        System.out.println("Others 2");
+        System.out.println("Others 2");
+        System.out.println("Others 2");
+
 
     }
 }
